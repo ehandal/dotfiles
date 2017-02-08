a43e7d5dd7a3fa874bb011cbdbb1bbdf1046ecf0
Edgar Handal <ehandal@gmail.com>
2017-02-08 13:00:28 -0600

    Actually commit setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+cd ~
+ln -si config/unix/bashrc.sh .bashrc
+ln -si config/unix/inputrc .inputrc
+ln -si config/vim/gvimrc.vim .gvimrc
+ln -si config/vim/vimrc.vim .vimrc
+ln -siT config/vim/vimfiles .vim
