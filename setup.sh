33c18bd3bfed039b67b312f70a0f43634aa1ebf3
Edgar Handal <ehandal@gmail.com>
2017-02-16 18:19:18 -0600

    Add zshrc

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ cd ~
 ln -si config/tmux.conf .tmux.conf
 ln -si config/unix/bashrc.sh .bashrc
 ln -si config/unix/inputrc .inputrc
+ln -si config/unix/zshrc .zshrc
 ln -si config/vim/gvimrc.vim .gvimrc
 ln -si config/vim/vimrc.vim .vimrc
 ln -siT config/vim/vimfiles .vim
