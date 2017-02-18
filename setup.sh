a4a568c910a531ddcc3cb176514af30fcc52c54a
Edgar Handal <ehandal@gmail.com>
2017-02-17 18:11:42 -0600

    Fix bashrc symlink

--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@
 set -e
 cd ~
 ln -si config/tmux.conf .tmux.conf
-ln -si config/bashrc.sh .bashrc
+ln -si config/bashrc .bashrc
 ln -si config/inputrc .inputrc
 ln -si config/zshrc .zshrc
 ln -si config/vim/gvimrc.vim .gvimrc
 ln -si config/vim/vimrc.vim .vimrc
+ln -si config/vimperatorrc .vimperatorrc
 ln -siT config/vim/vimfiles .vim
