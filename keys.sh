11ec365fc9eed6f4d416191f0176b3209648d7b6
Edgar Handal <ehandal@gmail.com>
2017-02-20 19:19:16 -0600

    Simplify keys.sh

--- keys.sh
+++ keys.sh
@@ -2,17 +2,8 @@
 # depends xmodmap xcape
 # https://github.com/alols/xcape
 
-# clear all mappings
-setxkbmap -option ''
 setxkbmap -option ctrl:nocaps
-
-# assign capslock to control on press, escape on release
-xmodmap -e 'clear Lock'
-xmodmap -e 'keycode 66 = Control_L'
-xmodmap -e 'add Control = Control_L'
-# make a fake escape key (so we can map it with xcape)
-xmodmap -e 'keycode 999 = Escape'
-xcape -e 'Control_L=Escape'
+xcape -e '#66=Escape'
 
 # assign return to control on press, return on release
 xmodmap -e 'keycode 36 = 0x1234'
