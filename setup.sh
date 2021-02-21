db35a6997451a627ef87249a2b4404a3da7bd47c
Edgar Handal <ehandal@gmail.com>
2021-02-21 04:37:09 -0600

    Use XDG config dir for tmux

--- setup.sh
+++ setup.sh
@@ -6,11 +6,11 @@ BASE=$(realpath --relative-to="$HOME" "$(dirname $BASH_SOURCE)")
 ln -siT $BASE/bashrc ~/.bashrc
 ln -siT $BASE/gitconfig ~/.gitconfig
 ln -siT $BASE/inputrc ~/.inputrc
-ln -siT $BASE/tmux.conf ~/.tmux.conf
 ln -siT $BASE/zshrc ~/.zshrc
 ln -siT $BASE/vim ~/.vim
 
-if [ ! -d ~/.tmux/plugins/tpm ]; then
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d ~/.config/tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 fi
-~/.tmux/plugins/tpm/bin/install_plugins
+ln -si ../../$BASE/tmux.conf ~/.config/tmux
+~/.config/tmux/plugins/tpm/bin/install_plugins
