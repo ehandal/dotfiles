0576b5c74988c4737ecb702abc2ce08348a41f9a
Edgar Handal <ehandal@gmail.com>
2021-02-22 13:20:44 -0600

    Use XDG config for git

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@ set -e
 
 BASE=$(realpath --relative-to="$HOME" "$(dirname $BASH_SOURCE)")
 
-mkdir -p ~/.config/tmux ~/.config/zsh
+mkdir -p ~/.config/{git,tmux,zsh}
+ln -siT ../../$BASE/gitconfig ~/.config/git/config
 ln -si ../../$BASE/inputrc ~/.config
 ln -si ../../$BASE/tmux.conf ~/.config/tmux
 ln -si ../../$BASE/zshrc ~/.config/zsh
 ln -siT ../../$BASE/zshrc ~/.config/zsh/.zshrc
 
 ln -siT $BASE/bashrc ~/.bashrc
-ln -siT $BASE/gitconfig ~/.gitconfig
 ln -siT $BASE/vim ~/.vim
 
 if [ ! -d ~/.config/tmux/plugins/tpm ]; then
