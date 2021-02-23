de3ff31aba19d971def6cf0a4d82577ca095d63c
Edgar Handal <ehandal@gmail.com>
2021-02-23 08:10:51 -0600

    Add nvim symlink to setup

--- setup.sh
+++ setup.sh
@@ -6,14 +6,15 @@ BASE=$(realpath --relative-to="$HOME" "$(dirname $BASH_SOURCE)")
 mkdir -p ~/.config/{git,tmux,zsh}
 ln -si ../$BASE/inputrc ~/.config
 ln -si ../$BASE/npmrc ~/.config
+ln -si ../$BASE/nvim ~/.config
 ln -si ../../$BASE/gitconfig ~/.config/git/config
 ln -si ../../$BASE/tmux.conf ~/.config/tmux
 ln -si ../../$BASE/zshrc ~/.config/zsh
 ln -si ../../$BASE/zshrc ~/.config/zsh/.zshrc
 
 ln -si $BASE/bashrc ~/.bashrc
-ln -si $BASE/vim ~/.vim
 ln -si $BASE/zshenv ~/.zshenv
+ln -siT $BASE/vim ~/.vim
 
 if [ ! -d ~/.config/tmux/plugins/tpm ]; then
     git clone https://github.com/tmux-plugins/tpm ~/.local/share/tmux/plugins/tpm
