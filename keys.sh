5a607f58e1ce3501210f2ed4abca42270a901b89
Edgar Handal <ehandal@gmail.com>
2021-03-09 19:42:52 -0600

    Improve key remapping

--- keys.sh
+++ keys.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 # depends xmodmap xcape
 # https://github.com/alols/xcape
+set -e
 
-setxkbmap -option ctrl:nocaps
-xcape -e '#66=Escape'
+setxkbmap -layout us -option # reset layout and options
+killall -q xcape || true # prevent multiple active xcapes
 
-# assign return to control on press, return on release
-xmodmap -e 'keycode 36 = 0x1234'
-xmodmap -e 'add Control = 0x1234'
-# make a fake return key (so we can map it with xcape)
-xmodmap -e 'keycode any = Return'
-xcape -e '0x1234=Return'
+setxkbmap -option ctrl:nocaps # CapsLock as Ctrl
+xcape -e '#66=Escape' # keycode 66 is CapsLock
+
+setxkbmap -option shift:both_capslock # both shifts together toggle capslock
+setxkbmap -option terminate:ctrl_alt_bksp # ctrl+alt+backspace to kill X server
+
+# assign Return to Control on press, Return on release
+xmodmap -e 'clear Lock'
+xmodmap -e 'clear Control'
+xmodmap -e 'keycode 36 = Control_R' # keycode 36 is Enter
+xmodmap -e 'keycode any = Return' # make a fake return key (so we can map it with xcape)
+xmodmap -e 'add Control = Control_L Control_R'
+xcape -e '#36=Return'
