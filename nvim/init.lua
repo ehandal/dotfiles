94b2e5413fd10fe699d32ce7f94e359ba69a90a5
Edgar Handal <ehandal@gmail.com>
2023-08-01 22:39:58 -0500

    nvim: enable treesitter indent if not large file

--- nvim/init.lua
+++ nvim/init.lua
@@ -104,18 +104,17 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     config = function()
+      local function is_large_file(_, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end
       require('nvim-treesitter.configs').setup { ---@diagnostic disable-line: missing-fields
         ensure_installed = {'c', 'cpp', 'lua', 'python', 'vim'},
-        highlight = {
-          enable = true,
-          disable = function(_, buf) -- disable on large files
-              local max_filesize = 100 * 1024 -- 100 KB
-              local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-              if ok and stats and stats.size > max_filesize then
-                  return true
-              end
-          end,
-        },
+        highlight = {enable = true, disable = is_large_file},
+        indent = {enable = true, disable = is_large_file},
       }
     end,
   },
