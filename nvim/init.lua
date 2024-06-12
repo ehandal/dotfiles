98665c872e02887f582d2bd2383dc6651264d604
Edgar Handal <ehandal@gmail.com>
2024-06-12 16:10:21 -0500

    nvim: use vim.uv instead of vim.loop

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,7 @@ vim.g.maplocalleader = ' '
 
 -- Install plugin manager
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system {
     'git',
     'clone',
@@ -112,7 +112,7 @@ require('lazy').setup({
     config = function()
       local function is_large_file(_, buf)
         local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
           return true
         end
