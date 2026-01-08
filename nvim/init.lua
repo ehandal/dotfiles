d311515ee28c0277afefe61a41a417f35955d4b7
Edgar Handal <ehandal@gmail.com>
2026-01-08 11:14:58 -0600

    nvim: use number highlights for diagnostics

--- nvim/init.lua
+++ nvim/init.lua
@@ -317,9 +317,15 @@ vim.api.nvim_create_autocmd('LspAttach', {group = lsp_cfg_augroup,
       virtual_lines = virtual_lines_opts,
       signs = {
         text = {
-          [vim.diagnostic.severity.ERROR] = '󰅚 ',
-          [vim.diagnostic.severity.WARN] = '󰀪 ',
-          [vim.diagnostic.severity.INFO] = '󰋽 ',
+          [vim.diagnostic.severity.ERROR] = '',
+          [vim.diagnostic.severity.WARN] = '',
+          [vim.diagnostic.severity.INFO] = '',
+          [vim.diagnostic.severity.HINT] = '',
+        },
+        numhl = {
+          [vim.diagnostic.severity.ERROR] = 'DiagnosticSignError',
+          [vim.diagnostic.severity.WARN] = 'DiagnosticSignWarn',
+          [vim.diagnostic.severity.INFO] = 'DiagnosticSignInfo',
           [vim.diagnostic.severity.HINT] = '',
         },
       },
