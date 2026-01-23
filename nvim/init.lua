bb9ebd7e7d28a6c8b851e0ff596e84d900f77271
Edgar Handal <ehandal@gmail.com>
2026-01-23 11:24:19 -0600

    nvim: add mapping to find hidden/ignored files

--- nvim/init.lua
+++ nvim/init.lua
@@ -95,6 +95,7 @@ require('lazy').setup({
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<Leader>fa', builtin.builtin)
       vim.keymap.set('n', '<Leader>ff', builtin.find_files)
+      vim.keymap.set('n', '<Leader>fF', function() builtin.find_files{hidden = true, no_ignore = true} end)
       vim.keymap.set('n', '<Leader>fg', builtin.live_grep)
       vim.keymap.set('n', '<Leader>fb', builtin.buffers)
       vim.keymap.set('n', '<Leader>fh', builtin.help_tags)
